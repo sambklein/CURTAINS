Stop the scaling and fix the plotting to run on the cluster.

data/physics_datasets.py
@@ -91,8 +91,8 @@ class Curtains(BasePhysics):
         # 'pt', 'eta', 'phi', 'mass', 'tau1', 'tau2', 'tau3', 'd12', 'd23', 'ECF2', 'ECF3'
         data[:, 0] = df['tau2'] / df['tau1']
         data[:, 1] = df['tau3'] / df['tau2']
-        data[:, 2] = df['d23']
-        data[:, 3] = df['d12']
+        data[:, 2] = df['d23']  # np.log(df['d23'] + 1)
+        data[:, 3] = df['d12']  # np.log(df['d12'] + 1)
         data[:, 4] = df['mass']
         return data, ['tau2s/taus', 'tau3s/tau2s', 'd23', 'd12', 'mass']
 

utils/plotting.py
@@ -93,8 +93,6 @@ def getFeaturePlot(model, original, sampled, lm_sample, nm, savedir, region, fea
     nfeatures = len(feature_names) - 1
     fig, axes = plt.subplots(nfeatures, nfeatures, figsize=(2 * nfeatures + 2, 2 * nfeatures - 1))
     sigcolour = ['red', 'blue']
-    signal_handle = [mpatches.Patch(color=colors) for colors in sigcolour]
-    signal_labels = ["Original", "Sampled"]
     for i in range(nfeatures):
         axes[i, 0].set_ylabel(feature_names[i])
         for j in range(nfeatures):
@@ -105,7 +103,7 @@ def getFeaturePlot(model, original, sampled, lm_sample, nm, savedir, region, fea
                 _, bins, _ = axes[i, j].hist(model.get_numpy(original[:, i]), bins=bin, density=True, histtype='step',
                                              color='red', label='Original')
                 axes[i, j].hist(model.get_numpy(sampled[:, i]), density=True, bins=bins, histtype='step', color='blue',
-                                label='Sampled')
+                                label='Transformed')
                 axes[i, j].hist(model.get_numpy(lm_sample[:, i]), density=True, bins=bins, histtype='step',
                                 color='black', linestyle='dashed', label='Input Sample')
 
@@ -179,6 +177,7 @@ def hist_features_single(originals, model, feature_nms, axs, bins, label='data')
 
 
 def plot_single_feature_mass_diagnostic(model, samples, generating_data, feature_names, sv_dir, title, nm):
+    print([generating_data[:, -1]] * int(samples.shape[0] / generating_data.shape[0]))
     generating_mass = torch.cat([generating_data[:, -1]] * int(samples.shape[0] / generating_data.shape[0]))
     nfeatures = samples.shape[1]
     fig, ax = plt.subplots(1, nfeatures, figsize=(5 * nfeatures + 2, 5))

utils/post_process.py
@@ -267,6 +267,7 @@ def post_process_anode(model, datasets, sup_title='NSF', quantiles=True):
 def post_process_curtains(model, datasets, sup_title='NSF'):
     low_mass_training = datasets.trainset.data1
     high_mass_training = datasets.trainset.data2
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     sv_dir = get_top_dir() + '/images' + '/' + model.dir
     if not os.path.exists(sv_dir):
@@ -291,10 +292,8 @@ def post_process_curtains(model, datasets, sup_title='NSF'):
             samples = model.transform_to_data(low_mass_sample[:nsamp],
                                               high_mass_sample[torch.randperm(s2, device=torch.device('cpu'))][:nsamp],
                                               batch_size=1000)
-        samples = high_mass_sample.unnormalize(samples)
-        hm = high_mass_sample.unnormalize(high_mass_sample.data)
         # For the feature plot we only want to look at as many samples as there are in SB1
-        getFeaturePlot(model, hm, samples[:nsamp], high_mass_sample.unnormalize(low_mass_sample.data), nm, sv_dir, set,
+        getFeaturePlot(model, high_mass_sample, samples[:nsamp], low_mass_sample, nm, sv_dir, set,
                        datasets.signalset.feature_nms)
 
     nmass = 5
@@ -310,8 +309,8 @@ def post_process_curtains(model, datasets, sup_title='NSF'):
     hist_features_single(lm, model, datasets.signalset.feature_nms, ax, bns, label='SB1')
     for mass in masses:
         with torch.no_grad():
-            samples = model.transform_to_data(low_mass_sample, mass * torch.ones((low_mass_sample.data.shape[0], 1)))
-        samples = low_mass_sample.unnormalize(samples)
+            samples = model.transform_to_data(low_mass_sample.data.to(device),
+                                              mass * torch.ones((low_mass_sample.data.shape[0], 1)).to(device))
         # getCrossFeaturePlot(model, low_mass_sample.unnormalize(low_mass_sample.data), samples, nm, sv_dir, mass,
         #                     datasets.signalset.feature_nms)
         hist_features_single(samples, model, datasets.signalset.feature_nms, ax, bns,
@@ -329,10 +328,12 @@ def post_process_curtains(model, datasets, sup_title='NSF'):
     for i in range(nshuffle):
         mass_sample = (min_mass - max_mass) * torch.rand(nsamp, 1) + max_mass
         with torch.no_grad():
-            samples[i] = model.transform_to_data(low_mass_sample[:nsamp], mass_sample)
+            samples[i] = model.transform_to_data(low_mass_sample[:nsamp], mass_sample.to(device))
 
     smp = low_mass_sample.unnormalize(samples.view(-1, nfeatures))
-    plot_single_feature_mass_diagnostic(model, smp, smp, datasets.signalset.feature_nms, sv_dir, 'Mass Diagnostic', nm)
+    print(smp)
+    plot_single_feature_mass_diagnostic(model, samples, smp, datasets.signalset.feature_nms, sv_dir,
+                                        'Mass Diagnostic', nm)
 
 
 def post_process_flows_for_flows(model, datasets, sup_title='NSF'):

validate_interpolation.py
@@ -12,7 +12,8 @@ from utils.hyperparams import get_measure
 
 from utils.training import fit
 
-from models.OT_models import curtains_transformer, tucan
+from models.OT_models import curtains_transformer, tucan, delta_mass_tucan, delta_tucan, \
+    delta_mass_curtains_transformer, delta_curtains_transformer
 from models.nn.flows import spline_flow, coupling_spline
 
 from utils import hyperparams
@@ -64,6 +65,8 @@ parser.add_argument('--gclip', type=int, default=None,
                     help='The value to clip the gradient by.')
 parser.add_argument('--nbins', type=int, default=10,
                     help='The number of bins to use in each spline transformation.')
+parser.add_argument('--ncond', type=int, default=2,
+                    help='The number of features to condition on.')
 
 ## reproducibility
 parser.add_argument('--seed', type=int, default=1638128,
@@ -114,22 +117,31 @@ tails = 'linear'  # This will ensure that any samples from outside of [-tail_bou
 if args.coupling:
     # TODO clean this up
     mx = [1] * int(np.ceil(datasets.nfeatures / 2)) + [0] * int(datasets.nfeatures - np.ceil(datasets.nfeatures / 2))
-    # TODO: should make a wrapper for this to be able to change the parameters
+
+
     # this has to be an nn.module that takes as first arg the input dim and second the output dim
-    # def set_params(inp_dim, maker):
-    maker = dense_net
+    def maker(input_dim, output_dim):
+        return dense_net(input_dim, output_dim, layers=[64, 64, 64], context_features=args.ncond)
+
+
     INN = coupling_spline(inp_dim, maker, nstack=args.nstack, tail_bound=tail_bound, tails=tails, lu=0,
                           num_bins=args.nbins, mask=mx)
 else:
     INN = spline_flow(inp_dim, args.nodes, num_blocks=args.nblocks, nstack=args.nstack, tail_bound=tail_bound,
                       tails=tails, activation=hyperparams.activations[args.activ], num_bins=args.nbins,
-                      context_features=2)
+                      context_features=args.ncond)
 
 # Build model
 if args.two_way:
-    transformer = tucan
+    if args.ncond == 2:
+        transformer = delta_mass_tucan  # tucan
+    else:
+        transformer = delta_tucan  # tucan
 else:
-    transformer = curtains_transformer
+    if args.ncond == 2:
+        transformer = delta_mass_curtains_transformer
+    else:
+        transformer = delta_curtains_transformer
 
 curtain_runner = transformer(INN, device, exp_name, measure, datasets.nfeatures, dir=args.d)
 