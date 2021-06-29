Added some plotting to the data loader, also changed the scaling to take the max as 99.99% of the data, which fixes our anomaly issues. Square canvas for the feature plots. New options for training the classifier for separating samples from transformations.

utils/DRE.py
@@ -29,8 +29,10 @@ class SupervisedDataClass(Dataset):
         return self.data.shape[0]
 
 
-def net(nfeatures, nclasses):
-    return dense_net(nfeatures, nclasses, layers=[64, 32, 16])
+def get_net(batch_norm=False, width=32, depth=2):
+    def net_maker(nfeatures, nclasses):
+        return dense_net(nfeatures, nclasses, layers=[width] * depth, batch_norm=batch_norm)
+    return net_maker
 
 
 def fit_classifier(classifier, train_data, valid_data, optimizer, batch_size, n_epochs, device, sv_dir, plot=True,
@@ -88,32 +90,48 @@ def fit_classifier(classifier, train_data, valid_data, optimizer, batch_size, n_
     print('Finished Training')
 
 
+def dope_data(truth, anomaly_data, beta):
+    n = int(len(truth) * (1 - beta))
+    n1 = len(truth) - n
+    truth = sample_data(truth, n)
+    anomaly_data, anomaly_data_train = sample_data(anomaly_data, n1, split=True)
+    try:
+        truth = torch.cat((anomaly_data_train, truth), 0)
+    except TypeError:
+        truth = np.concatenate((anomaly_data_train.detach().cpu(), truth), 0)
+        np.random.shuffle(truth)
+    return anomaly_data, truth
+
+
 def get_auc(interpolated, truth, directory, name, split=0.5, anomaly_data=None, mass_incl=True, balance=True,
-            beta=None, sup_title='', mscaler=None, load=False, return_rates=False):
+            beta=None, sup_title='', mscaler=None, load=False, return_rates=False, dope_splits=True):
     if balance:
         n = min((len(interpolated), len(truth)))
         interpolated = sample_data(interpolated, n)
         truth = sample_data(truth, n)
+
     sv_dir = directory + f'/{name}'
     anomaly_bool = anomaly_data is not None
     beta_bool = beta is not None
 
-    if beta_bool and anomaly_bool:
+    if beta_bool and anomaly_bool and (not dope_splits):
         if not anomaly_bool:
             print('No anomalies passed to DRE.')
         else:
-            n = int(len(truth) * (1 - beta))
-            n1 = len(truth) - n
-            truth = sample_data(truth, n)
-            anomaly_data_train, anomaly_data = sample_data(anomaly_data, n1, split=True)
-
-    if anomaly_bool:
-        truth = torch.cat((anomaly_data_train, truth), 0)
+            anomaly_data, truth = dope_data(truth, anomaly_data, beta)
+    elif anomaly_bool and (not beta_bool):
+        truth = torch.cat((anomaly_data, truth), 0)
 
     X, y = torch.cat((interpolated, truth), 0).cpu().numpy(), torch.cat(
         (torch.ones(len(interpolated)), torch.zeros(len(truth))), 0).view(-1, 1).cpu().numpy()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split, random_state=1, stratify=y)
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, random_state=1, stratify=y_train)
+
+    if beta_bool and anomaly_bool and dope_splits:
+        get_mask = lambda x: (x == 0).flatten()
+        anomaly_data, X_train[get_mask(y_train)] = dope_data(X_train[get_mask(y_train)], anomaly_data, beta)
+        anomaly_data, X_val[get_mask(y_val)] = dope_data(X_val[get_mask(y_val)], anomaly_data, beta)
+        anomaly_data, X_test[get_mask(y_test)] = dope_data(X_test[get_mask(y_test)], anomaly_data, beta)
 
     if mass_incl:
         test_mass = X_test[:, -1]
@@ -125,8 +143,11 @@ def get_auc(interpolated, truth, directory, name, split=0.5, anomaly_data=None, 
     valid_data = SupervisedDataClass(X_val, y_val)
     test_data = SupervisedDataClass(X_test, y_test)
 
-    batch_size = 100
+    batch_size = 64
     nepochs = 100
+    lr = 1e-4
+    wd = 0.001
+    net = get_net(batch_norm=True)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -134,7 +155,10 @@ def get_auc(interpolated, truth, directory, name, split=0.5, anomaly_data=None, 
                             activation=torch.sigmoid).to(device)
 
     # Make an optimizer object
-    optimizer = torch.optim.Adam(classifier.parameters(), lr=0.001)
+    if wd is not None:
+        optimizer = torch.optim.Adam(classifier.parameters(), lr=lr)
+    else:
+        optimizer = torch.optim.AdamW(classifier.parameters(), lr=lr, wd=wd)
 
     # Train
     if load:
@@ -201,14 +225,15 @@ def get_auc(interpolated, truth, directory, name, split=0.5, anomaly_data=None, 
             signal_mass = test_mass[mx][y_scores[mx] < threshold]
             if bins is None:
                 bins = get_bins(bg_mass, nbins=10)
-                norm_bg = np.sum(np.histogram(bg_mass, bins=bins)[0])
-                norm_signal = np.sum(np.histogram(signal_mass, bins=bins)[0])
+                norm_bg = np.histogram(bg_mass, bins=bins)[0]
+                norm_signal = np.histogram(signal_mass, bins=bins)[0]
             add_error_hist(ax[i], bg_mass, bins, 'blue', error_bars=True, label='Transformed jets', norm=norm_bg)
             signal_label = 'Signal window jets'
             add_error_hist(ax[i], signal_mass, bins, 'red', error_bars=True, label=signal_label, norm=norm_signal)
-            ax[i].set_yscale('log')
+            if i > 0:
+                ax[i].set_yscale('log')
             ax[i].set_xlabel('Mass (GeV)')
-            ax[i].set_title(f'BG rejection {at:.2f}')
+            ax[i].set_title(f'BG rejection {at:.2f} \n Yield {(signal_mass.shape[0] / bg_mass.shape[0]):.2f}')
         handles, labels = ax[0].get_legend_handles_labels()
         fig.legend(handles, labels, loc='upper right')
         fig.suptitle(sup_title)

utils/fitter_utils.py
@@ -1,6 +1,7 @@
 import zfit
 from zfit import z
 import math
+import torch
 
 
 class atlas_bg_PDF(zfit.pdf.ZPDF):
@@ -41,7 +42,7 @@ class linearPDF(zfit.pdf.ZPDF):
             limits = (self.scaler(limit) for limit in limits)
         sample = super().sample(*args, limits=limits, **kwargs)
         if self.scaler is not None:
-            sample = self.unscaler(sample)
+            sample = self.unscaler(torch.tensor(sample.numpy(), dtype=torch.float32))
         return sample
 
 

utils/plotting.py
@@ -9,7 +9,8 @@ from matplotlib import pyplot as plt  # , pyplot  # , pyplot
 # import matplotlib.patches as mpatches
 # from sklearn.manifold import TSNE
 # import seaborn as sns
-from utils.torch_utils import torch2numpy
+from utils.torch_utils import tensor2numpy
+from scipy import stats
 
 
 def get_bins(data, nbins=20):
@@ -122,19 +123,24 @@ def get_weights(data):
 def add_off_diagonal(axes, i, j, data, color):
     bini = get_bins(data[:, i])
     binj = get_bins(data[:, j])
-    f1 = torch2numpy(data[:, i])
-    f2 = torch2numpy(data[:, j])
-    axes[i, j].hist2d(f1, f2, bins=[bini, binj], density=True, cmap=color)
-    coef = np.corrcoef(f1, f2)
+    f1 = tensor2numpy(data[:, i])
+    f2 = tensor2numpy(data[:, j])
+    # axes[i, j].hist2d(f1, f2, bins=[bini, binj], density=True, cmap=color)
+    if i > j:
+        axes[i, j].hist2d(f1, f2, bins=[bini, binj], density=True, cmap=color)
+    else:
+        axes[i, j].hist2d(f2, f1, bins=[bini, binj], density=True, cmap=color)
     axes[i, j].set_xlim([-1, 1.1])
     axes[i, j].set_ylim([-1, 1.2])
-    axes[i, j].annotate(f'PC {coef[0, 1]:.2f}', xy=(0.95, 0.95), xycoords='axes fraction', ha='right', va='top', size=6)
+    # coef = np.corrcoef(f1, f2)[0, 1]
+    coef, pval = stats.spearmanr(f1, f2)
+    axes[i, j].annotate(f'SPR {coef:.2f}', xy=(0.95, 0.95), xycoords='axes fraction', ha='right', va='top', size=6)
     # bbox=dict(boxstyle='round', fc='w'), size=6)
 
 
 def getFeaturePlot(model, original, sampled, lm_sample, nm, savedir, region, feature_names, nbins=20):
     nfeatures = len(feature_names) - 1
-    fig, axes = plt.subplots(nfeatures, nfeatures, figsize=(2 * nfeatures + 2, 2 * nfeatures - 1))
+    fig, axes = plt.subplots(nfeatures, nfeatures, figsize=(2 * nfeatures + 2, 2 * nfeatures + 1))
     sigcolour = ['red', 'blue']
     for i in range(nfeatures):
         axes[i, 0].set_ylabel(feature_names[i])
@@ -191,18 +197,21 @@ def get_counts(data, to_slice, bound=4, nbins=50):
     return np.histogram2d(data[:, 0], data[:, 1], bins=bin_edges)[0]
 
 
-def hist_features(originals, sample, model, data_dim, axs, axs_nms=None):
+def hist_features(originals, sample, data_dim, axs, axs_nms=None, labels=None, legend=True):
+    if labels is None:
+        labels = ['original', 'samples']
     for i in range(data_dim):
         bins = get_bins(originals[:, i])
-        axs[i].hist(model.get_numpy(originals[:, i]), label='original', alpha=0.5, density=True, bins=bins,
+        axs[i].hist(tensor2numpy(originals[:, i]), label=labels[0], alpha=0.5, density=True, bins=bins,
                     histtype='step')
         # Plot samples drawn from the model
-        axs[i].hist(model.get_numpy(sample[:, i]), label='samples', alpha=0.5, density=True, bins=bins, histtype='step')
+        axs[i].hist(tensor2numpy(sample[:, i]), label=labels[1], alpha=0.5, density=True, bins=bins, histtype='step')
         if axs_nms:
             axs[i].set_title(axs_nms[i])
         else:
             axs[i].set_title('Feature {}'.format(i))
-        axs[i].legend()
+        if legend:
+            axs[i].legend()
 
 
 def hist_features_single(originals, model, feature_nms, axs, bins, label='data'):

utils/post_process.py
@@ -208,7 +208,7 @@ def post_process_anode(model, datasets, sup_title='NSF', quantiles=True):
         ncols = int(np.ceil(nfeatures / 4))
         fig, axs_ = plt.subplots(ncols, 4, figsize=(5 * 5 + 2, 5 * ncols))
         axs = fig.axes
-        hist_features(data_valid, valid_samples, model, nfeatures, axs, nms)
+        hist_features(data_valid, valid_samples, nfeatures, axs, nms)
         fig.suptitle(sv_name)
         fig.savefig(sv_dir + '/post_processing_{}_{}.png'.format(nm, sv_name))
 
@@ -268,7 +268,8 @@ def post_process_anode(model, datasets, sup_title='NSF', quantiles=True):
     return 0
 
 
-def post_process_curtains(model, datasets, sup_title='NSF', anomaly_data=None, load=False):
+def post_process_curtains(model, datasets, sup_title='NSF', anomaly_data=None, load=False, sample_mass=False):
+    # TODO: sample the mass!!
     low_mass_training = datasets.trainset.data1
     high_mass_training = datasets.trainset.data2
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -291,8 +292,7 @@ def post_process_curtains(model, datasets, sup_title='NSF', anomaly_data=None, l
         if lm > hm:
             raise Exception('First input must be the low end of the mass window.')
         data_mass = data.data[:, -1].view(-1, 1)
-        sample_mass = mass_sampler.sample(len(data_mass), limits=(lm.item(), hm.item())).numpy()
-        sample_mass = torch.tensor(sample_mass, dtype=torch.float32).to(model.device)
+        sample_mass = mass_sampler.sample(len(data_mass), limits=(lm.item(), hm.item())).to(model.device)
         if data_mass.min() >= hm:
             direction = 'inverse'
         elif data_mass.max() <= lm:
@@ -354,8 +354,10 @@ def post_process_curtains(model, datasets, sup_title='NSF', anomaly_data=None, l
     # Validation set one, SB2 to one mass bin higher
     get_maps('SB2', high_mass_training, {'OB2': datasets.validationset}, direction='forward')
     # AUC for OB2 vs T(SB2)
-    ob2_samples = get_samples(high_mass_training, datasets.validationset, 'forward', r_mass=True)
-    # sb2_samples = transform_to_mass(high_mass_sample, edge1, edge2)
+    if not sample_mass:
+        ob2_samples = get_samples(high_mass_training, datasets.validationset, 'forward', r_mass=True)
+    else:
+        ob2_samples = transform_to_mass(high_mass_sample, edge1, edge2)
     print('SB2 from OB2')
     auc_ob2 = get_auc(ob2_samples, datasets.validationset.data, sv_dir, nm + 'OB2_vs_TSB2',
                       mscaler=low_mass_training.unnorm_mass, load=load, sup_title=f'T(SB2) vs OB2')
@@ -364,7 +366,7 @@ def post_process_curtains(model, datasets, sup_title='NSF', anomaly_data=None, l
     get_maps('SB1', low_mass_training, {'OB1': datasets.validationset_lm}, direction='inverse')
     # AUC for OB1 vs T(SB1)
     ob1_samples = get_samples(low_mass_training, datasets.validationset_lm, 'inverse', r_mass=True)
-    # sb2_samples = transform_to_mass(high_mass_sample, edge1, edge2)
+    # ob1_samples = transform_to_mass(high_mass_sample, edge1, edge2)
     print('SB1 from OB1')
     auc_ob1 = get_auc(ob1_samples, datasets.validationset_lm.data, sv_dir, nm + 'OB1_vs_TSB1',
                       mscaler=low_mass_training.unnorm_mass, load=load, sup_title=f'T(SB1) vs OB1')
@@ -411,7 +413,8 @@ def post_process_curtains(model, datasets, sup_title='NSF', anomaly_data=None, l
 
     print('Without anomalies injected')
     auc = get_auc(samples, datasets.signalset.data, sv_dir, nm + 'SB12', mscaler=low_mass_training.unnorm_mass,
-                  load=load, sup_title=f'T(SB1) U T(SB2) vs SR')
+                  load=False, sup_title=f'T(SB1) U T(SB2) vs SR')
+
     with open(sv_dir + '/auc_{}.npy'.format(nm), 'wb') as f:
         np.save(f, auc_sb2)
         np.save(f, auc_sb1)
@@ -466,5 +469,5 @@ def post_process_flows_for_flows(model, datasets, sup_title='NSF'):
     sample = model.sample(low_mass_training.data.shape[0])
     nplot = 1
     fig, ax = plt.subplots(nplot, datasets.nfeatures, figsize=(5 * datasets.nfeatures + 2, 5 * nplot + 2))
-    hist_features(low_mass_training, sample, model, datasets.nfeatures, ax)
+    hist_features(low_mass_training, sample, datasets.nfeatures, ax)
     fig.savefig(sv_dir + '/post_processing_{}_{}.png'.format(nm, 'base_dist_sample'))